3a2ded38d3867529d67c6d1a9d8efba6d37a0cfe
atheo810 <atheos810@gmail.com>
2023-04-16 17:23:14 +0800

    updatin 25.Escaping Literal Quotes in Strings

--- javascript-algorithms-and-data-structures/Basic JavaScript/25.Escaping Literal Quotes in Strings/Escaping Literal Quotes in Strings.js
+++ javascript-algorithms-and-data-structures/Basic JavaScript/25.Escaping Literal Quotes in Strings/Escaping Literal Quotes in Strings.js
@@ -1,1 +1,1 @@
-const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line  
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line   
