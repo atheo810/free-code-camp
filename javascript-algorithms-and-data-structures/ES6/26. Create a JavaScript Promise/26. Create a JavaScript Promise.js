3a3a666ff6c24921fcc55a6db03307b23dc604f2
atheo810 <atheos810@gmail.com>
2023-04-25 10:39:35 +0800

    feat: 26

--- javascript-algorithms-and-data-structures/ES6/26. Create a JavaScript Promise/26. Create a JavaScript Promise.js
+++ javascript-algorithms-and-data-structures/ES6/26. Create a JavaScript Promise/26. Create a JavaScript Promise.js
@@ -1,12 +1,1 @@
-const makeServerRequest = new Promise((resolve, reject) => {
-  // responseFromServer represents a response from a server
-  let responseFromServer;
-
-  if (responseFromServer) {
-    // Change this line
-    resolve("We got the data");
-  } else {
-    // Change this line
-    reject("Data not received");
-  }
-});
+const makeServerRequest = new Promise((resolve, reject) => {});
