80a4ec61d7d13bbf0560e445f1381641eeacaead
atheo810 <atheos810@gmail.com>
2023-04-30 11:06:07 +0800

    feat: 11

--- javascript-algorithms-and-data-structures/Debugging/11.Use Caution When Reinitializing Variables Inside a Loop/Use Caution When Reinitializing Variables Inside a Loop.js
+++ javascript-algorithms-and-data-structures/Debugging/11.Use Caution When Reinitializing Variables Inside a Loop/Use Caution When Reinitializing Variables Inside a Loop.js
@@ -1,9 +1,9 @@
 function zeroArray(m, n) {
   // Creates a 2-D array with m rows and n columns of zeroes
   let newArray = [];
-  let row = [];
   for (let i = 0; i < m; i++) {
     // Adds the m-th row into newArray
+    let row = [];
 
     for (let j = 0; j < n; j++) {
       // Pushes n zeroes into the current row to create the columns
